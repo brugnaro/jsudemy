65edb2868bcafabdebdbbb24371b08d7b7520db6
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-06-05 23:06:02 -0300

    add audio in keyboard

--- calculator/js/controller/CalcController.js
+++ calculator/js/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor(){
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -36,10 +38,27 @@ class CalcController {
         }, 1000);
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio(){
+        if(this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard(){
         document.addEventListener('keyup', e => {
+            this.playAudio();        
             switch(e.key){
                 case 'Escape':
                     this.clearAll();
@@ -214,7 +233,7 @@ class CalcController {
     }
 
     execBtn(value){
-        
+        this.playAudio();
         switch(value){
             case 'ac':
                 this.clearAll();
@@ -313,6 +332,10 @@ class CalcController {
     }
 
     set displayCalc(value){
+        if(value.toString().length > 10){
+            this.setError();
+            return false;
+        }
         this._displayCalcEl.innerHTML = value; 
     }
 
