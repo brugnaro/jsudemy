8edf49e77cf7b4aab6beea69c969324ac48b39a4
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-06-08 01:30:31 -0300

    events in form

--- users-project/index.js
+++ users-project/index.js
@@ -1,2 +1,22 @@
-var name = document.querySelector('#exampleInputName');
-var gender = document.querySelector('#exampleInputGenderM');
+var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
+
+document.getElementById('form-user-create').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    fields.forEach((field, index)=>{
+        if(field.name == 'gender'){
+            if(field.checked) {
+                user[field.name] = field.value;
+            }
+        } else {
+            user[field.name] = field.value;
+        }
+    });
+    console.log(user);
+})
+
+/*document.querySelectorAll('button').forEach(()=>{
+    this.addEventListener('click',()=>{
+        console.log('clicou');
+    });
+});*/
