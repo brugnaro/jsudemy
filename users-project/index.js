7e81bb6625995d05252288c2552825e1b5b50ae3
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-06-10 20:39:12 -0300

    add User.js from model.

--- users-project/index.js
+++ users-project/index.js
@@ -2,8 +2,8 @@ var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
 addLine = (dataUser) => {
-    var tr = document.createElement('tr');
-    tr.innerHTML = `
+    console.log(dataUser);
+    document.getElementById('table-users').innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -15,8 +15,7 @@ addLine = (dataUser) => {
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
     </tr>
-    `
-    document.getElementById('table-users').appendChild(tr);
+    `;
 }
 
 document.getElementById('form-user-create').addEventListener('submit',(e)=>{
@@ -30,5 +29,6 @@ document.getElementById('form-user-create').addEventListener('submit',(e)=>{
             user[field.name] = field.value;
         }
     });
-    addLine(user);
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+    addLine(objectUser);
 });
