72a655de4732bbf5a5e6bfa7bc217a2bddef3cde
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-08-12 19:58:28 -0300

    working with events

--- users-management/index.js
+++ users-management/index.js
@@ -1,3 +1,16 @@
-var nome = document.querySelector('#exampleInputName');
-nome.value = 'marcos';
-nome.style.color = 'blue';
+var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+  event.preventDefault();
+  fields.forEach(function (field, index) {
+    if (field.name == 'gender') {
+      if (field.checked) {
+        user[field.name] = field.value;
+      }
+    } else {
+      user[field.name] = field.value;
+    }
+  });
+  console.log(user);
+});
+
