9517e475040891634f0bb3fa5cec6d4c78131a62
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-08-28 22:20:09 -0300

    Show and hide edit form.

--- users-management/controller/UserController.js
+++ users-management/controller/UserController.js
@@ -5,6 +5,13 @@ class UserController {
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
+    this.onEdit();
+  }
+
+  onEdit() {
+    document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+      this.showPanelCreate();
+    });
   }
 
   onSubmit() {
@@ -125,10 +132,15 @@ class UserController {
       <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
       <td>${Utils.dateFormat(dataUser.register)}</td>
       <td>
-        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
       </td>
   `;
+
+    tr.querySelector(".btn-edit").addEventListener("click", e => {
+      console.log(JSON.parse(tr.dataset.user));
+      this.showPanelUpdate();
+    });
 
     this.tableEl.appendChild(tr);
 
@@ -136,18 +148,28 @@ class UserController {
 
   }
 
+  showPanelCreate() {
+    document.querySelector("#box-user-create").style.display = "block";
+    document.querySelector("#box-user-update").style.display = "none";
+  }
+
+  showPanelUpdate() {
+    document.querySelector("#box-user-create").style.display = "none";
+    document.querySelector("#box-user-update").style.display = "block";
+  }
+
   updateCount() {
 
     let numberUsers = 0;
     let numberAdmin = 0;
 
-    [...this.tableEl.children].forEach(tr=>{
+    [...this.tableEl.children].forEach(tr => {
       numberUsers++;
       let user = JSON.parse(tr.dataset.user);
-      if(user._admin) numberAdmin++;
+      if (user._admin) numberAdmin++;
     });
 
-    console.log(numberUsers)
+    //console.log(numberUsers)
 
     document.querySelector("#number-users").innerHTML = numberUsers;
     document.querySelector("#number-users-admin").innerHTML = numberAdmin;
