834ddb2a7d900ba9c28fe3bb804e47cde4f2db98
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-08-31 22:45:22 -0300

    Refactoring template.

--- users-management/controller/UserController.js
+++ users-management/controller/UserController.js
@@ -24,7 +24,9 @@ class UserController {
       btn.disabled = true;
 
       let values = this.getValues(this.formUpdateEl);
+      
       let index = this.formUpdateEl.dataset.trIndex;
+      
       let tr = this.tableEl.rows[index];
 
       let userOld = JSON.parse(tr.dataset.user);
@@ -40,21 +42,11 @@ class UserController {
             result._photo = content;
           };
 
-          tr.dataset.user = JSON.stringify(result);
+          let user = new User();
 
-          tr.innerHTML = `
-          <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-          <td>${result._name}</td>
-          <td>${result._email}</td>
-          <td>${(result._admin) ? 'Sim' : 'Não'}</td>
-          <td>${Utils.dateFormat(result._register)}</td>
-          <td>
-            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-          </td>
-      `;
+          user.loadFromJSON(result);
 
-          this.addEventsTr(tr);
+          this.getTr(user, tr);
 
           this.updateCount();
 
@@ -186,8 +178,8 @@ class UserController {
 
     let users = [];
 
-    if (sessionStorage.getItem("users")) {
-      users = JSON.parse(sessionStorage.getItem("users"));
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
     }
 
     return users;
@@ -212,32 +204,40 @@ class UserController {
 
     users.push(data);
 
-    sessionStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("users", JSON.stringify(users));
   }
 
   addLine(dataUser) {
 
-    let tr = document.createElement('tr');
-
-    tr.dataset.user = JSON.stringify(dataUser);
-
-    tr.innerHTML = `
-      <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-      <td>${dataUser.name}</td>
-      <td>${dataUser.email}</td>
-      <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-      <td>${Utils.dateFormat(dataUser.register)}</td>
-      <td>
-        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-        <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
-      </td>
-  `;
-
-    this.addEventsTr(tr);
+    let tr = this.getTr(dataUser);
 
     this.tableEl.appendChild(tr);
 
     this.updateCount();
+
+  }
+
+  getTr(dataUser, tr = null) {
+
+    if (tr === null) tr = document.createElement("tr");
+
+    tr.dataset.user = JSON.stringify(dataUser);
+
+    tr.innerHTML = `
+    <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+    <td>${Utils.dateFormat(dataUser.register)}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
+    </td>
+    `;
+
+    this.addEventsTr(tr);
+
+    return tr;
 
   }
 
@@ -259,8 +259,6 @@ class UserController {
       for (let name in json) {
 
         let field = this.formUpdateEl.querySelector("[name=" + name.replace("_", "") + "]");
-
-
 
         if (field) {
 
